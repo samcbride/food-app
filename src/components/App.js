5f0a0bbc883ea0290f18a81d8de3906e5563839a
Shona McBride <mcbride.shona@gmail.com>
2021-03-21 17:07:03 +0000

    functionality changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,21 @@ import { useState } from "react";
 import "../styles/App.css";
 import Search from "./Search";
 import SearchResults from "./SearchResults";
+import fetchRecipes from "../requests/axiosrecipe";
 
 function App() {
   const [searchResults, setSearchResults] = useState([]);
+
   const getResults = async (searchInput) => {
-    const recipes = await searchInput;
+    const recipes = await fetchRecipes(searchInput);
     setSearchResults(recipes);
-    
-    };
+  };
+
   return (
     <div className="App">
-      What's in your cupboard?
-      <Search handleSubmit={getResults} />
-      <SearchResults searchResults={searchResults} />
+      <p className="title">What's in your cupboard?</p>
+      <Search className="search" handleSubmit={getResults} />
+      <SearchResults className="searchResults" searchResults={searchResults} />
     </div>
   );
 }
