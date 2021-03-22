7a811c858dcfad7eb29fb11f769a8745b8782489
Shona McBride <mcbride.shona@gmail.com>
2021-03-22 15:01:01 +0000

    added tests for Search component and updated path for App tests

--- src/testing/Search.test.js
+++ src/testing/Search.test.js
@@ -1,2 +1,23 @@
-import React from "react";
+import { render } from "@testing-library/react";
 import Search from "../components/Search";
+
+describe("Search", () => {
+  const validProps = {
+    handleSubmit: () => {},
+  };
+
+  it("renders correctly", () => {
+    const { asFragment } = render(
+      <Search handleSubmit={validProps.handleSubmit} />
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("check button exists with correct text", () => {
+    const { getByRole } = render(
+      <Search handleSubmit={validProps.handleSubmit} />
+    );
+    const button = getByRole("button", { name: /Add/i });
+    expect(button).toBeInTheDocument();
+  });
+});
