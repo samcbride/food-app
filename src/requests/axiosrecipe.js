f9ed54cfcdbada59a25b4497b0628df5bcc70895
Alex Segura <donalejandrobs@gmail.com>
2021-03-25 16:15:23 +0000

    More recipes (up to 20)

--- src/requests/axiosrecipe.js
+++ src/requests/axiosrecipe.js
@@ -2,11 +2,11 @@ import axios from "axios";
 const apiURL = "https://api.edamam.com/search?q=";
 const apiKey = "&app_key=8dc3f327e4b629fa54df8c1d47c5d0cf";
 const apiId = "&app_id=95c4ca1b";
-const maxTime = "&time=30";
+const maxHits = "&from=0&to=20";
 const maxIngreds = `&ingr=10`;
 
 const fetchRecipes = (ingredients) => {
-  const url = `${apiURL}${ingredients}${maxIngreds}${maxTime}${apiId}${apiKey}`;
+  const url = `${apiURL}${ingredients}${maxIngreds}${maxHits}${apiId}${apiKey}`;
   if (ingredients) {
     return axios
       .get(url)
