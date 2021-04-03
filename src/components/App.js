0e36afc422376b30af62ead8af6909951ffee358
Shona McBride <mcbride.shona@gmail.com>
2021-04-03 12:08:38 +0100

    fixed recipe search with ingredients only

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState, } from "react";
+import React, { useState } from "react";
 import Search from "./Search";
 import SearchResults from "./SearchResults";
 import FilterMenu from "./FilterMenu";
@@ -9,49 +9,42 @@ import "../styles/Random.css";
 import GetRandomRecipe from "./Random";
 import headerimage from "../images/headerimage.jpg";
 
-
-
 function App() {
   const [searchResults, setSearchResults] = useState([]);
 
   const [value, setValue] = useState();
-
 
   const getResults = async (searchInput) => {
     const recipes = await fetchRecipes(searchInput, value);
     setSearchResults(recipes);
   };
 
-  const getRandomResult = async ()=>{
+  const getRandomResult = async () => {
     const randomRecipe = await GetRandomRecipe();
-    setSearchResults(randomRecipe)
-  }
+    setSearchResults(randomRecipe);
+  };
 
-
-   return (
+  return (
     <div className="App">
       <div className="header">
         <img src={headerimage} alt="header-food-display"></img>
         <h1 className="title">What's in your cupboard?</h1>
       </div>
       <div className="wrapper">
-        <Search className="search" 
-         handleSubmit={getResults} 
-         setSearchResults={setSearchResults} 
+        <Search
+          className="search"
+          handleSubmit={getResults}
+          setSearchResults={setSearchResults}
         />
-      <FilterMenu value={value} setValue={setValue} />
+        <FilterMenu value={value} setValue={setValue} />
       </div>
 
       <SearchResults className="searchResults" searchResults={searchResults} />
-       <button
-      className="button"
-      onClick={getRandomResult}>
-      Random Recipe
+      <button className="button" onClick={getRandomResult}>
+        Random Recipe
       </button>
-       </div>
-
+    </div>
   );
 }
 
 export default App;
-
