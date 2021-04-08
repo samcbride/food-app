dad32b431ce0af67d83d600eeb39748aa1d47cf4
Shona McBride <mcbride.shona@gmail.com>
2021-04-08 20:25:51 +0100

    added one test to FilterMenu, some small spacing changes in SearchResults.test.js, added to state in App.js

--- src/testing/searchResults.test.js
+++ src/testing/searchResults.test.js
@@ -6,20 +6,17 @@ describe("SearchResults", () => {
   const validProps = {
     searchResults: [],
   };
-  
 
   it("renders correctly", () => {
-    const { asFragment } = render(<SearchResults 
-      searchResults={validProps.searchResults}
-    />);
+    const { asFragment } = render(
+      <SearchResults searchResults={validProps.searchResults} />
+    );
     expect(asFragment()).toMatchSnapshot();
   });
   it("renders the correct number of SearchResults instances", () => {
     const { getAllByTestId } = render(
-      <SearchResults 
-      searchResults={validProps.searchResults}
-      />
+      <SearchResults searchResults={validProps.searchResults} />
     );
     expect(getAllByTestId("search-results")).toHaveLength(1);
-  })
+  });
 });
